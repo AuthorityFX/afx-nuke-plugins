2d32300a10ec9c9bb8d5b9e1f35ef68201b8496d
Ryan P. Wilson <rpw@authorityfx.com>
2017-02-20 22:18:50 -0500

    Improved code in menu.py Set BOOST_ROOT default to /user/local/boost Missing "/" on icons cmake INSTALL command

--- menu.py
+++ menu.py
@@ -7,33 +7,31 @@
 #      Authority FX, Inc.
 #      www.authorityfx.com
 
+
 afx_plugin_path = os.path.dirname(os.path.realpath(__file__))
 nuke.pluginAddPath(os.path.join(afx_plugin_path, 'icons'))
-
 nuke.menu('Nodes').addMenu('Authority FX', icon='afx.png')
-for path in nuke.pluginPath():
 
-    if os.path.isfile(path + '/afx_soft_clip.so') == True:
-        nuke.menu('Nodes').addCommand('Authority FX/AFXSoftClip', lambda: nuke.createNode('AFXSoftClip'), icon='afx.png')
-        nuke.load('afx_soft_clip.so')
-    if os.path.isfile(path + '/afx_tone_map.so') == True:
-        nuke.menu('Nodes').addCommand('Authority FX/AFXToneMap', lambda: nuke.createNode('AFXToneMap'), icon='afx.png')
-        nuke.load('afx_tone_map.so')
-    if os.path.isfile(path + '/afx_median.so') == True:
-        nuke.knobDefault('AFXMedian.channels', 'alpha')
-        nuke.menu('Nodes').addCommand('Authority FX/AFXMedian', lambda: nuke.createNode('AFXMedian'), icon='afx.png')
-        nuke.load('afx_median.so')
-    if os.path.isfile(path + '/afx_chroma_key.so') == True:
-        nuke.menu('Nodes').addCommand('Authority FX/AFXChromaKey', lambda: nuke.createNode('AFXChromaKey'), icon='afx.png')
-        nuke.load('afx_chroma_key')
-    if os.path.isfile(path + '/afx_despill.so') == True:
-        nuke.menu('Nodes').addCommand('Authority FX/AFXDeSpill', lambda: nuke.createNode('AFXDeSpill'), icon='afx.png')
-        nuke.load('afx_despill.so')
-    if os.path.isfile(path + '/afx_anti_alias.so') == True:
-        nuke.menu('Nodes').addCommand('Authority FX/AFXAntiAlias', lambda: nuke.createNode('AFXAntiAlias'), icon='afx.png')
-        nuke.load('afx_anti_alias.so')
-        nuke.knobDefault('AFXAntiAlias.channels', 'alpha')
-    if os.path.isfile(path + '/afx_glow.so') == True:
-        nuke.menu('Nodes').addCommand('Authority FX/AFXGlow', lambda: nuke.createNode('AFXGlow'), icon='afx.png')
-        nuke.load('afx_glow.so')
-
+if os.path.isfile(os.path.join(afx_plugin_path, 'afx_soft_clip.so')):
+    nuke.menu('Nodes').addCommand('Authority FX/AFXSoftClip', lambda: nuke.createNode('AFXSoftClip'), icon='afx.png')
+    nuke.load('afx_soft_clip.so')
+if os.path.isfile(os.path.join(afx_plugin_path, 'afx_tone_map.so')):
+    nuke.menu('Nodes').addCommand('Authority FX/AFXToneMap', lambda: nuke.createNode('AFXToneMap'), icon='afx.png')
+    nuke.load('afx_tone_map.so')
+if os.path.isfile(os.path.join(afx_plugin_path, 'afx_median.so')):
+    nuke.knobDefault('AFXMedian.channels', 'alpha')
+    nuke.menu('Nodes').addCommand('Authority FX/AFXMedian', lambda: nuke.createNode('AFXMedian'), icon='afx.png')
+    nuke.load('afx_median.so')
+if os.path.isfile(os.path.join(afx_plugin_path, 'afx_chroma_key.so')):
+    nuke.menu('Nodes').addCommand('Authority FX/AFXChromaKey', lambda: nuke.createNode('AFXChromaKey'), icon='afx.png')
+    nuke.load('afx_chroma_key')
+if os.path.isfile(os.path.join(afx_plugin_path, 'afx_despill.so')):
+    nuke.menu('Nodes').addCommand('Authority FX/AFXDeSpill', lambda: nuke.createNode('AFXDeSpill'), icon='afx.png')
+    nuke.load('afx_despill.so')
+if os.path.isfile(os.path.join(afx_plugin_path, 'afx_anti_alias.so')):
+    nuke.menu('Nodes').addCommand('Authority FX/AFXAntiAlias', lambda: nuke.createNode('AFXAntiAlias'), icon='afx.png')
+    nuke.load('afx_anti_alias.so')
+    nuke.knobDefault('AFXAntiAlias.channels', 'alpha')
+if os.path.isfile(os.path.join(afx_plugin_path, 'afx_glow.so')):
+    nuke.menu('Nodes').addCommand('Authority FX/AFXGlow', lambda: nuke.createNode('AFXGlow'), icon='afx.png')
+    nuke.load('afx_glow.so')
