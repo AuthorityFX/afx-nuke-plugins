938ba412b097c16a54c462a4791cb23b21b35c18
Ryan P. Wilson <rpw@authorityfx.com>
2017-04-04 15:06:31 -0400

    Copy redist libs with symlinks at install

--- menu.py
+++ menu.py
@@ -12,29 +12,35 @@ afx_plugin_path = os.path.dirname(os.path.realpath(__file__))
 nuke.pluginAddPath(os.path.join(afx_plugin_path, 'icons'))
 nuke.menu('Nodes').addMenu('Authority FX', icon='afx.png')
 
-if os.path.isfile(os.path.join(afx_plugin_path, 'afx_soft_clip.so')):
+lib_extension = '.so'
+if os.name == 'nt':
+    lib_extension = '.dll'
+    # Windows has no RPATH equivalent - need to append lib dir to PATH variable
+    os.environ['PATH'] += os.pathsep + os.path.join(afx_plugin_path, 'lib')
+
+if os.path.isfile(os.path.join(afx_plugin_path, 'afx_soft_clip' + lib_extension)):
     nuke.menu('Nodes').addCommand('Authority FX/AFXSoftClip', lambda: nuke.createNode('AFXSoftClip'), icon='afx.png')
-    nuke.load('afx_soft_clip.so')
-if os.path.isfile(os.path.join(afx_plugin_path, 'afx_tone_map.so')):
+    nuke.load('afx_soft_clip' + lib_extension)
+if os.path.isfile(os.path.join(afx_plugin_path, 'afx_tone_map' + lib_extension)):
     nuke.menu('Nodes').addCommand('Authority FX/AFXToneMap', lambda: nuke.createNode('AFXToneMap'), icon='afx.png')
-    nuke.load('afx_tone_map.so')
-if os.path.isfile(os.path.join(afx_plugin_path, 'afx_median.so')):
+    nuke.load('afx_tone_map' + lib_extension)
+if os.path.isfile(os.path.join(afx_plugin_path, 'afx_median' + lib_extension)):
     nuke.knobDefault('AFXMedian.channels', 'alpha')
     nuke.menu('Nodes').addCommand('Authority FX/AFXMedian', lambda: nuke.createNode('AFXMedian'), icon='afx.png')
-    nuke.load('afx_median.so')
-if os.path.isfile(os.path.join(afx_plugin_path, 'afx_noise_map.so')):
+    nuke.load('afx_median' + lib_extension)
+if os.path.isfile(os.path.join(afx_plugin_path, 'afx_noise_map' + lib_extension)):
     nuke.menu('Nodes').addCommand('Authority FX/AFXNoiseMap', lambda: nuke.createNode('AFXNoiseMap'), icon='afx.png')
-    nuke.load('afx_noise_map.so')
-if os.path.isfile(os.path.join(afx_plugin_path, 'afx_chroma_key.so')):
+    nuke.load('afx_noise_map' + lib_extension)
+if os.path.isfile(os.path.join(afx_plugin_path, 'afx_chroma_key' + lib_extension)):
     nuke.menu('Nodes').addCommand('Authority FX/AFXChromaKey', lambda: nuke.createNode('AFXChromaKey'), icon='afx.png')
-    nuke.load('afx_chroma_key.so')
-if os.path.isfile(os.path.join(afx_plugin_path, 'afx_despill.so')):
+    nuke.load('afx_chroma_key' + lib_extension)
+if os.path.isfile(os.path.join(afx_plugin_path, 'afx_despill' + lib_extension)):
     nuke.menu('Nodes').addCommand('Authority FX/AFXDeSpill', lambda: nuke.createNode('AFXDeSpill'), icon='afx.png')
-    nuke.load('afx_despill.so')
-if os.path.isfile(os.path.join(afx_plugin_path, 'afx_anti_alias.so')):
+    nuke.load('afx_despill' + lib_extension)
+if os.path.isfile(os.path.join(afx_plugin_path, 'afx_anti_alias' + lib_extension)):
     nuke.menu('Nodes').addCommand('Authority FX/AFXAntiAlias', lambda: nuke.createNode('AFXAntiAlias'), icon='afx.png')
-    nuke.load('afx_anti_alias.so')
+    nuke.load('afx_anti_alias' + lib_extension)
     nuke.knobDefault('AFXAntiAlias.channels', 'alpha')
-if os.path.isfile(os.path.join(afx_plugin_path, 'afx_glow.so')):
+if os.path.isfile(os.path.join(afx_plugin_path, 'afx_glow' + lib_extension)):
     nuke.menu('Nodes').addCommand('Authority FX/AFXGlow', lambda: nuke.createNode('AFXGlow'), icon='afx.png')
-    nuke.load('afx_glow.so')
+    nuke.load('afx_glow' + lib_extension)
