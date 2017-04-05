0bf4ae3f34ae754e770c7a46bb81a70c855c2795
Ryan P. Wilson <rpw@authorityfx.com>
2017-04-05 11:25:36 -0400

    Add file for msvc compile

---
+++ include/msvc_hacks.h
@@ -0,0 +1,23 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Copyright (C) 2012-2016, Ryan P. Wilson
+//
+//      Authority FX, Inc.
+//      www.authorityfx.com
+
+#ifndef AFX_MSVC_HACKS_H_
+#define AFX_MSVC_HACKS_H_
+
+// MSVC10 does not have C99 support. fmin and fmax functions are not available
+#ifdef _WIN32
+  float fminf(float a, float b) {
+    return std::min(a, b);
+  }
+  float fmaxf(float a, float b) {
+    return std::max(a, b);
+  }
+#endif
+
+#endif  // AFX_MSVC_HACKS_H_
