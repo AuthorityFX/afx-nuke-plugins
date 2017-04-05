4eff8fd8880700ed523b73131b7bb7801469acb0
Ryan P. Wilson <rpw@authorityfx.com>
2017-04-05 11:27:19 -0400

    Copyright year

--- include/msvc_hacks.h
+++ include/msvc_hacks.h
@@ -2,7 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 //
-// Copyright (C) 2012-2016, Ryan P. Wilson
+// Copyright (C) 2017, Ryan P. Wilson
 //
 //      Authority FX, Inc.
 //      www.authorityfx.com
